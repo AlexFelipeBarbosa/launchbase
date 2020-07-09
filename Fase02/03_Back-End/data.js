fca9a62addf39348ba64c95ef3c89fe01e76a3ad
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-07-09 10:25:26 -0300

    Usando dados dinamicos

---
+++ Fase02/03_Back-End/data.js
@@ -0,0 +1,43 @@
+module.exports = [
+  {
+    id: 'BOWdx4rLwXg',
+    title: 'Migrando de Tecnologias Dentro de um App Grande',
+    duration: '45 min',
+    price: 'Free',
+  },
+
+  {
+    id: '7HMlkIBRH60',
+    title: 'Escalando Aplicações para Milhões de Usuários na Rede Globo',
+    duration: '33 min',
+    price: 'Free',
+  },
+
+  {
+    id: 't6s3sICLOM8',
+    title: 'Uso das Tecnologias em um Banco Digital - Banco Neon',
+    duration: '75 min',
+    price: 'Free',
+  },
+
+  {
+    id: 'x4FdZd2-_uU',
+    title: 'Recriando a Interface do Discord com ReactJS | UI Clone',
+    duration: '13 min',
+    price: 'Free',
+  },
+
+  {
+    id: 'P65RJTTqkN4',
+    title: 'Migrando de Tecnologias Dentro de um App Grande',
+    duration: '38 min',
+    price: 'Free',
+  },
+
+  {
+    id: 'bVAhNaxBEjM',
+    title: 'Debug de aplicações Node.js com VSCode | Code/Drops',
+    duration: '57 min',
+    price: 'Free',
+  },
+];
--- Fase02/03_Back-End/server.js
+++ Fase02/03_Back-End/server.js
@@ -2,25 +2,44 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 
 const server = express();
+const videos = require('./data.js');
 
 // arquivos estaticos
 server.use(express.static('public'));
 
 // configurando a template engine
-server.set('view engine', 'html');
+server.set('view engine', 'njk');
 
 nunjucks.configure('views', {
   express: server,
+  autoescape: false,
 });
 
 // adicionado a rota
 server.get('/', function (req, res) {
-  return res.render('index');
+  const about = {
+    avatar_url:
+      'https://avatars3.githubusercontent.com/u/12144620?s=460&u=b9785347e44440d8a08fbbaf61a72288c05671e0&v=4',
+    name: 'Alex Felipe Barbosa',
+    role: 'Analista de Sistemas / Desenvolvedor Backend',
+    description:
+      'Analista de Sistemas e Desenvolvedor SQL, conhecimentos em Oracle PL/SQL. <br> Mais informações acessar: <a href="http://www.alexbarbosa.info" target="_blanck">Blog</a>',
+    links: [
+      { name: 'Github', url: 'https://github.com/alexfelipebarbosa' },
+      { name: 'Twitter', url: 'https://twitter.com/alexf_barbosa' },
+      {
+        name: 'Linkedin',
+        url: 'https://www.linkedin.com/in/alexfelipebarbosa',
+      },
+      { name: 'Site', url: 'http://www.alexbarbosa.info' },
+    ],
+  };
+  return res.render('about', { about });
 });
 
 // Rota da pagina portifolio
 server.get('/portfolio', function (req, res) {
-  return res.render('portfolio');
+  return res.render('portfolio', { items: videos });
 });
 
 // servidor na porta 5000
