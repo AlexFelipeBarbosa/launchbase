b74f8a6d50b84c481664c2e4cd824911615bb13e
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-07-16 20:28:36 -0300

    Relacionando tabelas

--- Fase03/AcademiaBD/src/app/models/Instructor.js
+++ Fase03/AcademiaBD/src/app/models/Instructor.js
@@ -4,10 +4,13 @@ const db = require('../../config/db');
 module.exports = {
   all(callback) {
     db.query(
-      `
-              SELECT * 
-              FROM instructors
-              ORDER BY name ASC`,
+      ` 
+        SELECT instructors.*, count(members) as total_students
+        FROM instructors
+        LEFT JOIN members ON (instructors.id = members.instructor_id)
+        GROUP BY instructors.id
+        ORDER BY total_students DESC
+      `,
       function (err, results) {
         if (err) throw `Database Error! ${err}`;
 
--- Fase03/AcademiaBD/src/app/models/Member.js
+++ Fase03/AcademiaBD/src/app/models/Member.js
@@ -26,9 +26,10 @@ module.exports = {
           birth, 
           blood,
           weight,
-          height
+          height,
+          instructor_id
           )
-      VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
       RETURNING id
     `;
 
@@ -41,6 +42,7 @@ module.exports = {
       data.blood,
       data.weight,
       data.height,
+      data.instructor,
     ];
 
     db.query(query, values, function (err, results) {
@@ -52,9 +54,12 @@ module.exports = {
 
   find(id, callback) {
     db.query(
-      `SELECT * 
-              FROM members 
-              WHERE id = $1`,
+      `
+          SELECT members.*, instructors.name as instructor_name 
+          FROM members 
+          LEFT JOIN instructors 
+              ON (members.instructor_id = instructors.id)
+          WHERE members.id  = $1`,
       [id],
       function (err, results) {
         if (err) throw `Database Error! ${err}`;
@@ -73,8 +78,9 @@ module.exports = {
             email = ($5),
             blood = ($6),
             weight = ($7),
-            height = ($8)
-          WHERE id = $9    
+            height = ($8),
+            instructor_id = ($9)
+          WHERE id = $10    
     `;
 
     const values = [
@@ -86,6 +92,7 @@ module.exports = {
       data.blood,
       data.weight,
       data.height,
+      data.instructor,
       data.id,
     ];
 
@@ -107,6 +114,19 @@ module.exports = {
         if (err) throw `Database Error! ${err}`;
 
         return callback();
+      }
+    );
+  },
+
+  instructorsSelectOptions(callback) {
+    db.query(
+      `
+          SELECT name, id 
+          FROM instructors`,
+      function (err, results) {
+        if (err) throw `Database Error! ${err}`;
+
+        callback(results.rows);
       }
     );
   },
