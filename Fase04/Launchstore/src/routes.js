f1343b1d829e786b0d1b49bfd03fe013974ed6b7
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-07-20 21:42:20 -0300

    Layout basico da aplicação

--- Fase04/Launchstore/src/routes.js
+++ Fase04/Launchstore/src/routes.js
@@ -3,7 +3,7 @@ const routes = express.Router();
 
 // Rota "raiz do site"
 routes.get('/', function (req, res) {
-  return res.send('ok!S');
+  return res.render('layout.njk');
 });
 
 module.exports = routes;
