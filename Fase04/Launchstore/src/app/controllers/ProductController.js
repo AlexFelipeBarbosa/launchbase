89354535507a94b1a180a957caeac5c876ed23af
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-07-25 16:18:05 -0300

    Tratando Erros com Try/catch

--- Fase04/Launchstore/src/app/controllers/ProductController.js
+++ Fase04/Launchstore/src/app/controllers/ProductController.js
@@ -1,7 +1,7 @@
 const { formatPrice } = require('../../lib/utils');
 const Category = require('../models/Category');
 const Product = require('../models/Product');
-const { put } = require('../../routes');
+const File = require('../models/File');
 
 module.exports = {
   create(req, res) {
@@ -27,10 +27,19 @@ module.exports = {
         return res.send('Please, fill all fields!');
       }
     }
+
+    if (req.files.length == 0)
+      return res.send('Please, send at least one image');
+
     let results = await Product.create(req.body);
     const productId = results.rows[0].id;
 
-    return res.redirect(`/products/${productId}`);
+    const filesPromise = req.files.map((file) =>
+      File.create({ ...file, product_id: productId })
+    );
+    await Promise.all(filesPromise);
+
+    return res.redirect(`/products/${productId}/edit`);
   },
 
   async edit(req, res) {
@@ -42,8 +51,20 @@ module.exports = {
     product.old_price = formatPrice(product.old_price);
     product.price = formatPrice(product.price);
 
+    // Pegando as Categorias
     results = await Category.all();
     const categories = results.rows;
+
+    // Pegando as Imagens
+    results = await Product.files(product.id);
+    let files = results.rows;
+    files = files.map((file) => ({
+      ...file,
+      src: `${req.protocol}://${req.headers.host}${file.path.replace(
+        'public',
+        ''
+      )}`,
+    }));
 
     return res.render('products/edit.njk', { product, categories });
   },
@@ -52,10 +73,21 @@ module.exports = {
     const keys = Object.keys(req.body);
 
     for (key of keys) {
-      if (req.body[key] == '') {
+      if (req.body[key] == '' && key != 'removed_files') {
         return res.send('Please, fill all fields!');
       }
     }
+
+    if (req.body.removed_files) {
+      const removedFiles = req.body.removed_files.split(',');
+      const lastIndex = removedFiles.length - 1;
+      removedFiles.splice(lastIndex, 1);
+
+      const removedFilesPromise = removedFiles.map((id) => File.delete(id));
+
+      await Promise.all(removedFilesPromise);
+    }
+
     // Alterando o price do formato do front para gravar no back (deixando somente números)
     req.body.price = req.body.price.replace(/\D/g, '');
 
@@ -67,5 +99,11 @@ module.exports = {
     await Product.update(req.body);
 
     return res.redirect(`/products/${req.body.id}/edit`);
+  },
+
+  async delete(req, res) {
+    await Product.delete(req.body.id);
+
+    return res.redirect('/products/create');
   },
 };
