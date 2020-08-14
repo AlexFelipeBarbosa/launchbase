b514a53d2b7330c3e91d591bd1290dca606104f4
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-08-14 10:38:19 -0300

    Seeds de usuarios com Faker

--- Fase05/LaunchstoreAvancado/src/app/models/Product.js
+++ Fase05/LaunchstoreAvancado/src/app/models/Product.js
@@ -15,7 +15,7 @@ module.exports = {
     return results.rows;
   },
 
-  search(params) {
+  async search(params) {
     const { filter, category } = params;
 
     let query = '',
@@ -41,7 +41,8 @@ module.exports = {
             ${filterQuery}
             `;
 
-    return db.query(query);
+    const results = await db.query(query);
+    return results.rows;
   },
 };
 
