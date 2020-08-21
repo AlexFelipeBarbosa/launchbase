47dd769a8c86330b331db8496937dbe0c71a025b
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-08-21 14:31:19 -0300

    Animações com Lottie e Mensagens pós Pedido

--- Fase05/LaunchstoreAvancado/src/routes/users.js
+++ Fase05/LaunchstoreAvancado/src/routes/users.js
@@ -38,4 +38,6 @@ routes.get('/ads', UserController.ads);
 
 routes.post('/orders', onlyUsers, OrderController.post);
 
+routes.get('/orders', (req, res) => res.render('orders/error'));
+
 module.exports = routes;
